66ac299bd3e545faab639fe7383b18b14c7ebea4
relative-rene <rene.arellano415@gmail.com>
2018-04-12 11:06:51 -0700

    adds icon cdn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ export class AppComponent implements OnInit {
 
   constructor() { }
 
-
   ngOnInit() {
     this.googleLinkPath = "https://www.google.com/maps/dir//Neptune's,+630+Central+Ave,+Alameda,+CA+94501/@37.7713103,-122.2803176,17z/data=!4m15!1m6!3m5!1s0x808f81259556cefd:0xcf256f444dae8579!2sNeptune's!8m2!3d37.7713061!4d-122.2781289!4m7!1m0!1m5!1m1!1s0x808f81259556cefd:0xcf256f444dae8579!2m2!1d-122.2781289!2d37.7713061";
     this.navIsFixed = false;
@@ -33,9 +32,11 @@ export class AppComponent implements OnInit {
   }
 
   scrollDown(e) {
-    console.log('e ', e);
     e.scrollIntoView({behavior:'smooth', block:'start'});
+  }
 
+  dropMenu(e) {
+    console.log('e ', e);
   }
 }
 
